c20305acd0031002ba52267f17a6d5bda468771f
Yidu-git <yman88.yt@gmail.com>
2026-05-03 11:56:37 +0300

    UPDATE + TEST ROUTE

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,3 +4,9 @@ import { getHealth } from '../controllers/health.js';
 export const router = Router();
 
 router.get('/health', getHealth);
+router.get('/users', (req, res) => {
+ res.json({ message: 'List of users would go here' });
+});
+router.get("/test", (req, res) => {
+ res.json({ message: "Test route is working!" });
+});
